b6ed5e4b11ae8f9ac8750352df446677761113eb
yunrry <yunryy329@gmail.com>
2025-07-23 14:12:45 +0900

    fix : update User

--- src/main/java/backend/airo/domain/user/User.java
+++ src/main/java/backend/airo/domain/user/User.java
@@ -24,7 +24,7 @@ public class User {
     @Column(nullable = false)
     private String email;
 
-    @Column(nullable = false)
+    @Column(nullable = true)
     private String name;
 
     @Column(nullable = false, unique = true)
