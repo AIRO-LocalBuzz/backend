49069ba5e81b0f248440b5c688873cc0c5477d8e
이종원 <whddnjs5255@gmail.com>
2025-07-23 23:29:40 +0900

    feature: 축제 및 행사, 소상공인 정보 수집 기능 구현/배치 시스템 구현

---
+++ src/main/java/backend/airo/infra/clure_fatvl_open_api/dto/OpenApiResponse.java
@@ -0,0 +1,37 @@
+package backend.airo.infra.clure_fatvl_open_api.dto;//package garbege.open_api.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+//소상공인시장진흥공단 상가 정보는 이게 맞음.
+//TODO 전국문화축제 정보는 아직 모름 확인 필요
+public record OpenApiResponse<T>(
+        @JsonProperty("response") Response<T> response) {
+
+    public record Response<T>(
+            @JsonProperty("header") Header header,
+            @JsonProperty("body") Body<T> body) {
+
+        public record Header(
+                @JsonProperty("description") String description,
+                @JsonProperty("columns")     String columns,
+                @JsonProperty("stdrYm")      String stdrYm,
+                @JsonProperty("resultCode")  String resultCode,
+                @JsonProperty("resultMsg")   String resultMsg) {}
+
+        @JsonIgnoreProperties(ignoreUnknown = true)
+        public record Body<T>(
+                @JsonProperty("items") List<T> items,
+                @JsonProperty("numOfRows") int numOfRows,
+                @JsonProperty("pageNo")    int pageNo,
+                @JsonProperty("totalCount") long totalCount) {
+
+            public record List<T>(
+                    @JsonProperty("item") T item) {}
+        }
+    }
+
+    public T getItems() {
+        return response.body.items.item;
+    }
+}
