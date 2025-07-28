ab98afa7dbabe2c5f2f03b0a36e58e537994d249
이종원 <whddnjs5255@gmail.com>
2025-07-28 12:27:38 +0900

    fix: 소상공인 정보 List Request에서 Region 필드 삭제

--- src/main/java/backend/airo/api/shop/dto/ShopListResponse.java
+++ src/main/java/backend/airo/api/shop/dto/ShopListResponse.java
@@ -1,17 +1,12 @@
 package backend.airo.api.shop.dto;
 
-import backend.airo.domain.clure_fatvl.ClutrFatvl;
 import lombok.Builder;
-
-import java.time.LocalDate;
-import java.util.List;
 
 @Builder
 public record ShopListResponse(
 
         Long id,
         String name,
-        String region,
         String lot,
         String road,
         String indeScleName
